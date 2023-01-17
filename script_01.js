6563e0fea5e5f78b729f148d75b1510cd6b3a881
poisonpanicyvy <poisonpanicyvy@web.de>
2023-01-17 10:41:55 +0100

    new change

--- script_01.js
+++ script_01.js
@@ -4,12 +4,26 @@
 
 // 1. Kapselung von Codeblöcken
 
-test();
+// test();
+
 
 // Funktionsrumpf (body) | callee
 // Funktionsdeklaration
 function test()
 
 {
-console.log("Hallo Patrick");
+console.log("Hallo Poisonpanicyvy");
+}
+
+// Funktion 2 
+// Parametrisierung + Datenübergabe von innen
+
+ausgabeNamen();
+
+function ausgabeNamen() {
+    console.log("Hallo Poisonpanicyvy");
+    console.log("Hallo martin");
+ console.log("Hallo Stefan");
+    console.log("Hallo Peter");
+
 }
